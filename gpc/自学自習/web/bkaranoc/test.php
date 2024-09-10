940f543f1ac17b349031beba7e5a2611dd73b27a
takut368 <44548546+takut368@users.noreply.github.com>
2024-09-10 16:55:27 +0900

    Update test.php

--- gpc/自学自習/web/bkaranoc/test.php
+++ gpc/自学自習/web/bkaranoc/test.php
@@ -1,23 +1,11 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>Redirect</title>
-    <script>
-        window.onload = function() {
-            // 3秒後に外部サイトリンクBに遷移
-            setTimeout(function() {
-                window.location.href = "https://www.google.com/";
-            }, 3000); // 3秒後
-
-            // 8秒後に外部サイトリンクCに遷移
-            setTimeout(function() {
-                window.location.href = "https://translate.google.com/";
-            }, 8000); // 8秒後
-        };
-    </script>
-</head>
-<body>
-    <p>リンクBに自動遷移し、その後リンクCに自動遷移します。</p>
-</body>
-</html>
+<script>
+    window.onload = function() {
+        // 新しいタブでサイトBを開く
+        var siteB = window.open("https://www.google.com/", "_blank");
+        
+        // 5秒後にサイトCに遷移
+        setTimeout(function() {
+            window.location.href = "https://translate.google.com/";
+        }, 5000); // 5秒後
+    };
+</script>
